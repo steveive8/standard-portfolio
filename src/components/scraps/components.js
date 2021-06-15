5533f14cea459f87254f653aa998677cdfa4cd21
johnsteve108 <buddhychoi@gmail.com>
2021-06-15 18:30:25 +0900

    scraps

--- src/components/scraps/components.js
+++ src/components/scraps/components.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {Col, Row, Flex, Text, Image} from '../common/plain';
+import {Close} from '../../images/close';
 
 const Box = styled(Flex)`
     border-radius: 20px;
@@ -16,30 +17,32 @@ const Box = styled(Flex)`
     margin: 10px;
 `;
 
-export const ScrapBox = ({setScrap, index, data}) => {
+export const ScrapBox = ({setScrap, setOn, on, index, data}) => {
     const {title, description, src} = data;
     return (
-        <Box onClick={() => setScrap(index)}>
+        <Box onClick={() => {setScrap(data); setOn(!on)}}>
             <Image src={src} width="100%" height="100%" of="cover" />
             <Col padding="padding: 10px" bg="rgba(0,0,0,0.85)" style={{width: '100%', height: '50%', position: 'absolute', bottom: 0}}>
-                <Text color="white" weight="700">{title}</Text>
+                <Text color="white" weight="700" lh={21}>{title}</Text>
                 <Text lh="20" color="white" margin="margin-top: 10px">{description}</Text>
             </Col>
         </Box>
     )
 }
 
-export const Slide = ({scrap, setScrap, data = {title: "", description: "", src: ""}, article}) => {
-    const {title, description, src} = data;
+export const Slide = ({on, setOn, scrap = {title: "", description: "", src: ""}, article}) => {
+    const {title, description, src, date} = scrap;
     return (
-        <Col width="25%" height="100%" position="fixed" bg="rgb(10,10,10)" zIndex={1002} style={{opacity: scrap ? 1 : 0, right: scrap ? '0%' : '-25%', transition: 'all 300ms', top: 0}}>
+        <Col width="30%" height="100%" position="fixed" bg="rgb(10,10,10)" zIndex={1002} style={{opacity: on ? 1 : 0, right: on ? '0%' : '-30%', transition: 'all 300ms', top: 0, overflowY: 'scroll'}}>
             <Row padding="padding: 15px;">
-                <Text color="white" weight="700">{title ? title : null}</Text>
-                <Text color="white" onClick={() => setScrap(false)}>닫기</Text>
+                <Text color="white" size={21} lh={25} weight="700">{title ? title : null}<Text color="rgb(200,200,200)" size={15} margin="margin-left: 10px" weight={"500"}>{date ? date : null}</Text></Text>
+                <Flex onClick={() => setOn(false)} to="cursor" position="absolute" style={{right: 10, top: 17}}>
+                    <Close size={20} />
+                </Flex>
             </Row>
             <Image src={src ? src : null} width="100%" height="220px" of="cover" />
             <Col padding="padding: 15px;">
-                <Text color="white" lh="20">{description ? description : null}</Text>
+                <Text color="white" lh="25">{description ? description : null}</Text>
             </Col>
         </Col>
     )
---
+++ src/images/close.js
@@ -0,0 +1,11 @@
+export const Close = ({size = 54}) => (
+    <svg width={size} height={size} viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" opacity="0.583649408">
+            <g id="Group-21">
+                <circle id="Oval" fill="#4D4D4D" cx="27" cy="27" r="27"></circle>
+                <line x1="13.5" y1="13" x2="40.5" y2="40" id="Path-50" stroke="#FFFFFF" stroke-width="3"></line>
+                <line x1="13.5" y1="13" x2="40.5" y2="40" id="Path-50" stroke="#FFFFFF" stroke-width="3" transform="translate(27.000000, 26.500000) scale(-1, 1) translate(-27.000000, -26.500000) "></line>
+            </g>
+        </g>
+    </svg>
+)
